176b0d7c921ed9bb4c0891b2932775e89197c5b7
Nady Alwazah <nadysy.7@gmail.com>
2026-06-02 01:11:15 +0300

    feat(splash): add splash navigation logic and create initial SigninView

--- lib/core/app/routes.dart
+++ lib/core/app/routes.dart
@@ -1,10 +1,13 @@
+import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/signin_view.dart';
 import 'package:flutter_task06_travel_app_beg/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+  static const String kSignin = '/signin';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
+      GoRoute(path: kSignin, builder: (context, state) => const SigninView()),
     ],
   );
 }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/core/app/routes.dart';
 import 'package:flutter_task06_travel_app_beg/core/theme/app_colors.dart';
 import 'package:flutter_task06_travel_app_beg/core/utils/assets.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    executeNavigation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,5 +24,12 @@ class SplashViewBody extends StatelessWidget {
       color: AppColors.primary,
       child: Center(child: Image.asset(AssetsData.splashImage)),
     );
+  }
+
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
+      context.go(AppRouter.kSignin);
+    });
   }
 }
