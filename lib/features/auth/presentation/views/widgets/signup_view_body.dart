02ec71cb0f4b1c088d1533a7a14dbb4038799210
Nady Alwazah <nadysy.7@gmail.com>
2026-06-03 02:16:07 +0300

    feat(auth/signup): add SocialIconsRow to SignupViewBody

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -4,6 +4,7 @@ import 'package:flutter_task06_travel_app_beg/core/widgets/custom_button.dart';
 import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/auth_header.dart';
 import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/have_an_account_widget.dart';
+import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/social_icons_row.dart';
 
 class SignupViewBody extends StatefulWidget {
   const SignupViewBody({super.key});
@@ -79,6 +80,12 @@ class _SignupViewBodyState extends State<SignupViewBody> {
 
               const SizedBox(height: 32),
               const HaveAnAccountWidget(),
+
+              const SizedBox(height: 16),
+              const Text("Or connect", style: TextStyle(color: Colors.grey)),
+
+              const SizedBox(height: 42),
+              const SocialIconsRow(),
             ],
           ),
         ),
