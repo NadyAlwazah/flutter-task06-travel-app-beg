e950e726f770e626c3c8e8676a33a0033eeecb33
Nady Alwazah <nadysy.7@gmail.com>
2026-06-03 00:00:06 +0300

    fix(ui): hide status bar in SplashView by adding AppBar

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/core/theme/app_colors.dart';
 import 'package:flutter_task06_travel_app_beg/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
@@ -6,6 +7,9 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SplashViewBody());
+    return Scaffold(
+      appBar: AppBar(backgroundColor: AppColors.primary),
+      body: const SplashViewBody(),
+    );
   }
 }
