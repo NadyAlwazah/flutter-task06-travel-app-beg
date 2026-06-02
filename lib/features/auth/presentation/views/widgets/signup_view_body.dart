09ff52509ba4f96a5fce555fef9c5e3975b51a73
Nady Alwazah <nadysy.7@gmail.com>
2026-06-03 00:24:47 +0300

    feat(auth/signup): add AuthHeader to SignupViewBody

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/auth_header.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            const AuthHeader(
+              title: 'Sign up now',
+              subtitle: 'Please fill the details and create account',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
