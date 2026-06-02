f76189c348a34defa21cd180916d4d4e76b3843e
Nady Alwazah <nadysy.7@gmail.com>
2026-06-02 18:15:47 +0300

    feat(auth): add reusable CustomTextFormField and integrate it into Signin form

--- lib/features/auth/presentation/views/widgets/signin_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/auth_header.dart';
+import 'package:flutter_task06_travel_app_beg/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 
-class SigninViewBody extends StatelessWidget {
+class SigninViewBody extends StatefulWidget {
   const SigninViewBody({super.key});
 
   @override
+  State<SigninViewBody> createState() => _SigninViewBodyState();
+}
+
+class _SigninViewBodyState extends State<SigninViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            AuthHeader(
-              title: 'Sign in now',
-              subtitle: 'Please sign in to continue our app',
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: const Column(
+            children: [
+              SizedBox(height: 32),
+              AuthHeader(
+                title: 'Sign in now',
+                subtitle: 'Please sign in to continue our app',
+              ),
+
+              SizedBox(height: 32),
+              CustomTextFormField(
+                hintText: "Email",
+                textInputType: TextInputType.emailAddress,
+              ),
+
+              SizedBox(height: 24),
+              CustomTextFormField(
+                hintText: "Password ",
+                textInputType: TextInputType.text,
+                obscureText: true,
+              ),
+            ],
+          ),
         ),
       ),
     );
