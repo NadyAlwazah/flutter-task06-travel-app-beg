b7b5938fbf0d3e45cf2772f3f98f092baf75ae0e
Nady Alwazah <nadysy.7@gmail.com>
2026-06-03 23:47:19 +0300

    feat(home): add CustomAppBar and initial header section to HomeView

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/core/widgets/custom_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //AppBar
+        CustomAppBar(title: "Popular Places"),
+
+        SizedBox(height: 16),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            "All Popular Places",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+          ),
+        ),
+      ],
+    );
   }
 }
