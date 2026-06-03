cd88520c1132ca135b3c51fb16d40978a1a0c5d2
Nady Alwazah <nadysy.7@gmail.com>
2026-06-04 01:46:51 +0300

    feat(popular-packages): add CustomAppBar and initial PopularPackagesViewBody structure

--- lib/features/popular_packages/presentation/views/popular_packages_view.dart
+++ lib/features/popular_packages/presentation/views/popular_packages_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/core/widgets/custom_app_bar.dart';
+import 'package:flutter_task06_travel_app_beg/features/popular_packages/presentation/views/widgets/popular_packages_view_body.dart';
 
 class PopularPackagesView extends StatelessWidget {
   const PopularPackagesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //AppBar
+        CustomAppBar(title: "Popular Packages"),
+
+        SizedBox(height: 16),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            "Popular Trip Package",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+          ),
+        ),
+
+        // Body
+        Expanded(child: PopularPackagesViewBody()),
+      ],
+    );
   }
 }
---
+++ lib/features/popular_packages/presentation/views/widgets/popular_packages_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PopularPackagesViewBody extends StatelessWidget {
+  const PopularPackagesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
