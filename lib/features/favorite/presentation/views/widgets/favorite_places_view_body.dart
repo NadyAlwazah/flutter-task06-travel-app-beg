ad9df65908080ca5352f6bbe0f643054a34d3a2c
Nady Alwazah <nadysy.7@gmail.com>
2026-06-04 12:02:16 +0300

    feat(favorite): setup initial favorite places screen with CustomAppBar and body widget

---
+++ lib/features/favorite/presentation/views/widgets/favorite_places_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritePlacesViewBody extends StatelessWidget {
+  const FavoritePlacesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
