bfd851a9a108e5a548838d98019959ea6a1a26c0
Nady Alwazah <nadysy.7@gmail.com>
2026-06-08 23:04:00 +0300

    feat(profile): add ProfileCard to EditProfileViewBody

--- lib/features/profile/presentation/views/widgets/edit_profile_view_body.dart
+++ lib/features/profile/presentation/views/widgets/edit_profile_view_body.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task06_travel_app_beg/core/utils/assets.dart';
+import 'package:flutter_task06_travel_app_beg/features/profile/presentation/views/widgets/profile_card.dart';
 
 class EditProfileViewBody extends StatelessWidget {
   const EditProfileViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            Center(
+              child: ProfileCard(
+                name: "N",
+                subTitle: "email",
+                isEmail: false,
+                // name: user.fullName,
+                // subTitle: user.email,
+                imagePath: AssetsData.accountAvatarSvg1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
